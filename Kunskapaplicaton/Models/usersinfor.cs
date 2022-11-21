b0ab187bd556b32efb652365d0d109c5d296c0a0
Niloofar22 <niloorahi.se@gmail.com>
2022-11-21 15:13:22 +0100

    new changes uppdate and delete

--- Kunskapaplicaton/Models/usersinfor.cs
+++ Kunskapaplicaton/Models/usersinfor.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Kunskapaplicaton.Models
 {
+    [Table("usersinfor")]
     public class usersinfor
     {
+        [Column ("id")]
+        
         public int id { get; set; }
+        [Column("first_name")]
         public string first_name { get; set; }
+        [Column("last_name")]
         public string last_name { get; set; }
+        [Column("title")]
         public string title { get; set; }
         // vi flyttade från uaddress till här för att det är lättare och alla skulle vara i en class
     
